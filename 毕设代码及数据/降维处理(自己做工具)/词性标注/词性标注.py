a5e9c1770d1f2c78854bca7f5f6d73c8ad315213
wangye8899 <3091485316@qq.com>
2019-04-29 22:18:59 +0800

    词性标注

--- 毕设代码及数据/降维处理(自己做工具)/词性标注/词性标注.py
+++ 毕设代码及数据/降维处理(自己做工具)/词性标注/词性标注.py
@@ -21,7 +21,7 @@ def Mongo_process(wy):
     函数说明：读取mongodb数据，数据存储list
     wy:数据库读取数量
     """
-
+    comments_pro= []
     comments_list = []     
     connect = MongoClient('localhost',27017)
     db = connect.douban
@@ -44,8 +44,8 @@ def Mongo_process(wy):
             # 评论数据汇总至List中，进行下一步操作——词频统计、词云生成
             comments_list.append(info['comment_info'])
             # TFIDF(info['comment_info'],5,True)
-            wordsproperty(info['comment_info']) 
-
+            comments_pro.append(wordsproperty(info['comment_info'])) 
+    return comments_pro
 def Stopwords_process(infostr):
     """
     去除停用词
@@ -56,7 +56,7 @@ def Stopwords_process(infostr):
         if word not in stopwords:
             outstr+=word
             # outstr+=""
-    print(outstr)
+    # print(outstr)
     return outstr
 
 def Jieba_process(infostr):
@@ -93,11 +93,35 @@ def ReHtml_process(infostr):
 
 def wordsproperty(comment):
     words = pseg.cut(comment)
+    f = open('词性标注.txt','a')
     for word ,flag in words:
-        if word ==' ':
+        if word == " ":
             pass
-        elif :
-            
-    
+        # 先不做过滤处理，直接将词性标注写到文件
+        else:
+            f.write(str(word)+"  "+str(flag))
+            f.write("\n")
+    f.write("##################################################")
+    f.write("\n")
+    f.close()
+    words1 = pseg.cut(comment)
+    # 根据词性过滤掉不符合要求的词语
+    commentpro = []
+    comments_all = ""
+    for word1 ,flag1 in words1:
+        if word1 == " ":
+            pass
+        else:
+            # print(flag1)
+            # 保留动词（V）、区别词（b）、副词（d）、状态词（z）、状态词（zg）、形容词（a）
+            if "v" in str(flag1) or "b" in str(flag1) or "d" in str(flag1) or "z" in str(flag1) or "zg" in str(flag1) or "a" in str(flag1):
+                print(flag1)
+                comments_all+=str(word1)+" "
+    return comments_all 
+
+
 if __name__ == "__main__":
-    Mongo_process(1)
+    final_comments = []
+    # 去除不符合要求的词之后的评论
+    final_comments = Mongo_process(1000)
+    print(final_comments)
